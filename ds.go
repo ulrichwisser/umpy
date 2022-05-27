3a03c346cfeff5da8631a06671c4de44c52e4f3d
Ulrich Wisser <ulrich@wisser.se>
2022-05-28 00:33:02 +0200

    Added test for labels with DS have to be delegated

--- ds.go
+++ ds.go
@@ -16,17 +16,23 @@ func checkDS(cache Cache, origin string) (r Result) {
 		for _, rr := range cache[label]["DS"] {
 			ds := rr.(*dns.DS)
 			if !okDigestType(ds.DigestType) {
-				if viper.GetInt("verbose") > 0 {
+				if viper.GetInt(VERBOSE) >= VERBOSE_ERROR {
 					fmt.Printf("Label %s has DS record with forbidden digest type %s (%d)\n", ds.Header().Name, hash2string(ds.DigestType), ds.DigestType)
 				}
 				r.errors += 1
 			}
 			if !okAlgorithm(ds.Algorithm) {
-				if viper.GetInt("verbose") > 0 {
+				if viper.GetInt(VERBOSE) >= VERBOSE_ERROR {
 					fmt.Printf("Label %s has DS record with forbidden algorithm %s (%d)\n", ds.Header().Name, algorithm2string(ds.Algorithm), ds.Algorithm)
 				}
 				r.errors += 1
 			}
+		}
+		if _,ok:=cache[label]["NS"]; !ok {
+			if viper.GetInt(VERBOSE) >= VERBOSE_ERROR {
+				fmt.Printf("Label %s has DS record but is not delegated.\n", label)
+			}
+			r.errors += 1
 		}
 	}
 	return

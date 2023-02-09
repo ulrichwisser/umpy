4f550abcb439c8162a03078a51d82a65b58163c9
Ulrich Wisser <ulrich@wisser.se>
2023-02-09 21:15:13 +0100

    changed to log module

--- soa.go
+++ soa.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/apex/log"
 	"github.com/miekg/dns"
 	"github.com/spf13/viper"
 )
@@ -10,9 +9,7 @@ import (
 func checkSOA(cache Cache, origin string) (r Result) {
 	soa, ok := cache[origin]["SOA"][0].(*dns.SOA)
 	if !ok {
-		if viper.GetInt(VERBOSE) >= VERBOSE_ERROR {
-			fmt.Printf("No SOA record found.\n")
-		}
+		log.Errorf("No SOA record found.\n")
 		r.errors = 1
 		return
 	}
@@ -21,9 +18,7 @@ func checkSOA(cache Cache, origin string) (r Result) {
 	minvalid := uint32(viper.GetInt(MINVALID))
 
 	if soa.Expire >= minvalid {
-		if viper.GetInt(VERBOSE) >= VERBOSE_ERROR {
-			fmt.Printf("SOA Expire %d is too long. Must be less then %s %d\n", soa.Expire, MINVALID, minvalid)
-		}
+		log.Errorf("SOA Expire %d is too long. Must be less then %s %d\n", soa.Expire, MINVALID, minvalid)
 		r.errors = 1
 	}
 	return

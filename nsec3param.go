bafae8e3c79863dbe882bff08e48856e7137b7b1
Ulrich Wisser <ulrich@wisser.se>
2023-02-09 23:59:10 +0100

    switch to new logger

--- nsec3param.go
+++ nsec3param.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/apex/log"
 	"github.com/miekg/dns"
 	"github.com/spf13/viper"
 )
@@ -11,49 +10,35 @@ import (
 
 func checkNSEC3PARAM(cache Cache, origin string) (r Result) {
 	if _, ok := cache[origin]["NSEC3PARAM"]; !ok {
-		if viper.GetInt("verbose") > 0 {
-			fmt.Println("No NSEC3PARAM record found at apex.")
-		}
+		log.Error("No NSEC3PARAM record found at apex.")
 		r.errors += 1
 		return
 	}
 	if len(cache[origin]["NSEC3PARAM"]) > 1 {
-		if viper.GetInt("verbose") > 0 {
-			fmt.Printf("Found %d NSEC3PARAM records at apex.\n", len(cache[origin]["NSEC3PARAM"]))
-		}
+		log.Errorf("Found %d NSEC3PARAM records at apex.", len(cache[origin]["NSEC3PARAM"]))
 		r.errors += 1
 		return
 	}
 	nsec3param := cache[origin]["NSEC3PARAM"][0].(*dns.NSEC3PARAM)
 	if nsec3param.Hash != dns.SHA1 {
-		if viper.GetInt("verbose") > 0 {
-			fmt.Printf("NSEC3PARAM hash algorithm must specify SHA1 (1), value is %d.\n", nsec3param.Hash)
-		}
+		log.Errorf("NSEC3PARAM hash algorithm must specify SHA1 (1), value is %d.", nsec3param.Hash)
 		r.errors += 1
 	}
 	if nsec3param.Flags != 0 {
-		if viper.GetInt("verbose") > 0 {
-			fmt.Printf("NSEC3PARAM flags should be zero, value is %d.\n", nsec3param.Flags)
-		}
+		log.Errorf("NSEC3PARAM flags should be zero, value is %d.", nsec3param.Flags)
 		r.errors += 1
 	}
 
 	if int(nsec3param.Iterations) > viper.GetInt(NSEC3_MAXITERATIONS) {
-		if viper.GetInt("verbose") > 0 {
-			fmt.Printf("NSEC3PARAM iterations should be zero, value is %d. Values above %d are possibly treated as bogus.\n", nsec3param.Iterations, viper.GetInt(NSEC3_MAXITERATIONS))
-		}
+		log.Errorf("NSEC3PARAM iterations should be zero, value is %d. Values above %d are possibly treated as bogus.", nsec3param.Iterations, viper.GetInt(NSEC3_MAXITERATIONS))
 		r.errors += 1
 	} else if nsec3param.Iterations != 0 {
-		if viper.GetInt("verbose") > 0 {
-			fmt.Printf("NSEC3PARAM iterations should be zero, value is %d.\n", nsec3param.Iterations)
-		}
+		log.Errorf("NSEC3PARAM iterations should be zero, value is %d.", nsec3param.Iterations)
 		r.warnings += 1
 	}
 
 	if nsec3param.SaltLength != 0 || nsec3param.Salt != "" {
-		if viper.GetInt("verbose") > 0 {
-			fmt.Println("NSEC3PARAM salt should not be used.")
-		}
+		log.Error("NSEC3PARAM salt should not be used.")
 		r.warnings += 1
 	}
 	return

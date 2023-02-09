31a55fd0ce612c2dd83cf58c446544ef50f1dd21
Ulrich Wisser <ulrich@wisser.se>
2023-02-10 00:00:00 +0100

    switch to new logger

--- nsec.go
+++ nsec.go
@@ -1,26 +1,20 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/apex/log"
 	"github.com/miekg/dns"
-	"github.com/spf13/viper"
 )
 
 func checkNSEC(cache Cache, origin string) (r Result) {
 	var nseclabels []string = getNsecLabels(cache, origin)
 	for i := range nseclabels {
 		if _, ok := cache[nseclabels[i]]["NSEC"]; !ok {
-			if viper.GetInt("verbose") > 0 {
-				fmt.Printf("Label %s does not have a NSEC record\n", nseclabels[i])
-			}
+			log.Errorf("Label %s does not have a NSEC record", nseclabels[i])
 			r.errors += 1
 			continue
 		}
 		if len(cache[nseclabels[i]]["NSEC"]) > 1 {
-			if viper.GetInt("verbose") > 0 {
-				fmt.Printf("Label %s has %d NSEC records, only one expected.\n", nseclabels[i], len(cache[nseclabels[i]]["NSEC"]))
-			}
+			log.Errorf("Label %s has %d NSEC records, only one expected.", nseclabels[i], len(cache[nseclabels[i]]["NSEC"]))
 			r.errors += 1
 		}
 		nsec := cache[nseclabels[i]]["NSEC"][0].(*dns.NSEC)
@@ -29,9 +23,7 @@ func checkNSEC(cache Cache, origin string) (r Result) {
 			nextindex = 0
 		}
 		if nsec.NextDomain != nseclabels[nextindex] {
-			if viper.GetInt("verbose") > 0 {
-				fmt.Printf("NSEC record for label %s has %s as next domain. expected %s\n", nseclabels[i], nsec.NextDomain, nseclabels[nextindex])
-			}
+			log.Errorf("NSEC record for label %s has %s as next domain. expected %s", nseclabels[i], nsec.NextDomain, nseclabels[nextindex])
 			r.errors += 1
 		}
 	}
